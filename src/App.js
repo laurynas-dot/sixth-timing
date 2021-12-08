842e38770ac8eafd667b4866791c55886f811343
Laurynas <laurynas.tam@outlook.com>
2021-12-08 22:49:14 +0200

    Implement replay button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,57 @@
+import React, { Component } from "react";
+import timingR1 from "./assets/timingR1.mp3"
 
+class App extends Component {
+  state = {
+    audio: new Audio(timingR1),
+    isPlaying: false
+  };
 
-function App() {
+  isRushing = true;
+  health = 5;
+  
+  setup = () => {
+    if (this.state.audio)
+      this.state.audio.pause();
 
-  return (
+    this.state.audio = timingR1;
+    this.isRushing = true;
+    this.replay();
+  }
+
+  replay = () => {
+
+    const isPlaying = this.state.isPlaying;
+
+    this.state.audio.currentTime = 0;
+    this.state.audio.play();
+
+    this.setState({ isPlaying: !isPlaying });
+  };
+
+  proccessCorrect = () => {
+
+  }
+
+  proccessIncorrect = () => {
+    --this.health;
+  }
+
+  onRush = () => {
+    if (this.isRushing)
+      this.proccessCorrect();
+    else 
+      this.proccessIncorrect();
+  }
+
+  onLag = () => {
+    if (!this.isRushing)
+      this.proccessCorrect();
+    else 
+      this.proccessIncorrect();
+  }
+
+  render() { return(
     <div className="App">
       <div className="timmingPage">
         <div className="topBar">
@@ -17,12 +66,13 @@ function App() {
         </div>
         <div className="controls">
           <button>Rushing</button>
-          <button>Replay</button>
+          <button onClick={this.replay}>Replay</button>
           <button>Draging</button>
         </div>
       </div>
     </div>
   );
+  }
 }
 
 export default App;
